0d2ce7e39189b691b119d85bfbf0069140cb9a2c
Shallon Kobusinge <50081035+shallonkobusinge@users.noreply.github.com>
2024-08-19 21:44:31 +0200

    fix: display

---
+++ HeadStarter/flashcard-saas/app/api/generate/route.js
@@ -0,0 +1,100 @@
+// import { NextResponse } from "next/server";
+// import OpenAI from "openai";
+
+// const systemPrompt = `
+// You are a flashcard creator.
+
+// You will be given a topic and you will be asked to generate flashcards based on the topic.
+// 1. Create clear and concise flashcards for the topic.
+// 2. Provide accurate and informative answers on the back of the flashcard
+// 3. Ensure that each flashcard focuses on a specific topic and that each flashcard is relevant to the topic
+// 4. Use clear language to make the flashcards accessible to a wide range of learners
+// 5. Include a variety of question types, such as definitions, examples, comparisons and applications.
+// 6. Avoid orverly complex or ambiguous phrasing in both questions and answers
+// 7. When appropriate, use mnenomics or memory aids to help remember the questions and answers and reinforce the information.
+// 8. Taller the difficulty level of the flashcards to the user's specified preferences.
+// 9. If given a body of text, extract the most important and relevant information for the flashcards.
+// 10. Aim to create a balanced set of flashcards that covers the topic comprehensively.
+
+// Return in the following JSON format:
+
+// {
+//     "flashcards": [
+//         {
+//             "front": "Question 1",
+//             "back": "Answer 1"
+//         },
+//         {
+//             "front": "Question 2",
+//             "back": "Answer 2"
+//         },
+//         {
+//             "front": "Question 3",
+//             "back": "Answer 3"
+//         },
+//         {
+//             "front": "Question 4",
+//             "back": "Answer 4"
+//         },
+//         {
+//             "front": "Question 5",
+//             "back": "Answer 5"
+// }
+
+// `
+
+// export async function POST(req){
+//     const openai = OpenAI()
+//     const data = await req.text()
+
+//     const completion = await openai.chat.completion.create({
+//         messages: [
+//             {role: "system", content: systemPrompt},
+//             {role: "user", content: data},
+//         ],
+//         model : "gpt-4o",
+//         response_format: {type: "json_object"},
+
+//     })
+
+//     const flashcards = JSON.parse(completion.choices[0].message.content)
+//     return NextResponse.json(flashcards.flashcard)
+// }
+
+
+import { NextResponse } from 'next/server'
+import OpenAI from 'openai'
+
+const systemPrompt = `
+You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 10 flashcards.
+Both front and back should be one sentence long.
+You should return in the following JSON format:
+{
+  "flashcards":[
+    {
+      "front": "Front of the card",
+      "back": "Back of the card"
+    }
+  ]
+}
+`
+
+export async function POST(req) {
+    const openai = new OpenAI()
+    const data = await req.text()
+  
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: 'system', content: systemPrompt },
+        { role: 'user', content: data },
+      ],
+      model: 'gpt-4o',
+      response_format: { type: 'json_object' },
+    })
+  
+    // Parse the JSON response from the OpenAI API
+    const flashcards = JSON.parse(completion.choices[0].message.content)
+  
+    // Return the flashcards as a JSON response
+    return NextResponse.json(flashcards.flashcards)
+  }
--- HeadStarter/flashcard-saas/firebase.js
+++ HeadStarter/flashcard-saas/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirebase } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -13,9 +12,12 @@ const firebaseConfig = {
   storageBucket: "ai-flashcard-c307a.appspot.com",
   messagingSenderId: "175969756223",
   appId: "1:175969756223:web:83ea87150cbb01d7c6048a",
-  measurementId: "G-QCCB18V40D"
+  measurementId: "G-QCCB18V40D",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirebase(app);
+
+export { db };
